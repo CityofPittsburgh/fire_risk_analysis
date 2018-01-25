afb842834bfe84d1fcc01a0348b6d41ccf4a9505
mmadaio <mmadaio@cs.cmu.edu>
2018-01-25 17:59:48 -0500

    Add data dashboard R Shiny files

---
+++ server.R
@@ -0,0 +1,195 @@
+# Data dashboard for Metro21 Fire Risk Analysis Project
+# Created for: Pittsburgh Bureau of Fire
+# Authors: Qianyi Hu, Michael Madaio
+# Latest update: January 25, 2018
+
+
+# The server side of the dashboard
+
+#install.packages("shiny")
+#install.packages("ggplot2")
+#install.packages("dplyr")
+#install.packages("plotly")
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+
+
+
+shinyServer(function(input, output) {
+  
+  # read data  
+  model <- read.csv("data/fire_risk_nonres.csv")   
+
+  model$Score <- ceiling(model$RiskScore*10)
+  print(model$Pgh_FireDistrict)
+  
+  # get data subset
+  data <- reactive({
+    # default option: select all
+   
+    print("Filtering Fire Risk Scores")
+
+    d <- subset(model, subset = (Score <= input$range[2] & Score >= input$range[1]))
+
+    # filter by property type (STATEDESC)
+    if (!("All Classification Types" %in% input$property)){
+      d <- subset(d, subset=(STATEDESC %in% input$property))
+    }
+    
+    # filter by usage type (USEDESC)
+    if (!("All Usage Types" %in% input$use)) {
+      d <- subset(d, subset=(USEDESC %in% input$use))
+    }
+    
+    # filter by neighborhood (NEIGHDESC)
+    if (!("All Neighborhoods" %in% input$nbhood)) {
+      d <- subset(d, subset=(geo_name_nhood %in% input$nbhood))
+    }
+    # filter by fire district
+    if (!("All Fire Districts" %in% input$fire_dst)){
+      d <- subset(d, subset=(Pgh_FireDistrict %in% input$fire_dst)) 
+    }
+    d
+  
+     
+    
+  })
+  
+  #### Visualization output  ####
+  plot_Vis <- reactive({
+    
+      if (input$xvar == "Property Classification") {
+        x_axis <- "STATEDESC"
+      } else if (input$xvar == "Property Usage Type") {
+        x_axis <- "USEDESC"
+      } else if (input$xvar == "Neighborhood") {
+        x_axis <- "hood_x"
+      }
+      
+      if (input$yvar == "Fire Risk Scores") {
+        if (input$xvar == "Fire District") {
+        x_axis <- "Pgh_FireDistrict"
+      } 
+     
+    } 
+    
+    y_axis <- input$yvar
+
+    
+    
+    
+    ## Create visualization ##
+    
+    if (y_axis == "Fire Risk Scores"){
+      print("displaying Fire risk")
+      
+     
+      # consider average risk score by x axis
+      if (nlevels(data()[[x_axis]]) <= 15){
+        plot <- ggplot(data = data()[!is.na(data()[[x_axis]]),],aes(x=data()[!is.na(data()[[x_axis]]),][[x_axis]],y=Score)) + 
+          stat_summary(fun.y = "mean",geom = "bar",width=0.8,fill="steelblue") + 
+          stat_summary(aes(label=..y..),fun.y = function(x){round(mean(x),2)},geom = "text",size=5,vjust=-1,width=0.8) +
+          theme(plot.title = element_text(size = 18, face = "bold"),text = element_text(size=15)) +
+          ggtitle("Average Risk Score") + ylim(0,10) + 
+          xlab(x_axis) +
+          ylab("Risk Score")
+        
+      }else{
+        data_selected <- data()[!is.na(data()[[x_axis]]),]
+        
+        ag_score <- aggregate(data_selected[["Score"]] ~ data_selected[[x_axis]], data_selected, mean)
+        ag_label <- as.vector(unlist(ag_score[order(ag_score[2]),][1]))
+        print(length(ag_label))
+        # h = 550 + 10 * length(ag_label)
+        plot <- ggplot(data = data_selected, aes(x=data_selected[[x_axis]],y=Score)) + 
+          stat_summary(fun.y = "mean",geom = "bar",width=0.8,fill="steelblue") + 
+          coord_flip() + scale_x_discrete(limits=ag_label,labels=ag_label) +
+          stat_summary(aes(label=..y..),fun.y = function(x){round(mean(x),2)},geom = "text",size=4,hjust=-1) +
+          ggtitle("Average Risk Score") + ylim(0,10) + 
+          theme(plot.title = element_text(size = 18, face = "bold"),text = element_text(size=14)) +
+          xlab(x_axis) +
+          ylab("Risk Score")
+      }
+      
+    }
+    plot
+  })
+
+  
+
+    
+  ## Allow for variable height of visualization, dependent on number of records ##
+  
+  myHeight <- function(){
+    
+    if (input$yvar == "Fire Risk Scores") {
+      
+        if (input$xvar == "Property Classification") {
+          x_axis <- "STATEDESC"
+        } else if (input$xvar == "Property Usage Type") {
+          x_axis <- "USEDESC"
+        } else if (input$xvar == "Neighborhood") {
+          x_axis <- "hood_x"
+        } else if (input$xvar == "Fire District") {
+          x_axis <- "Pgh_FireDistrict"
+        } 
+        data_selected <- data()[!is.na(data()[[x_axis]]),]
+      
+        ag_score <- aggregate(data_selected[["Score"]] ~ data_selected[[x_axis]], data_selected, mean)
+        ag_label <- as.vector(unlist(ag_score[order(ag_score[2]),][1]))
+    
+    } 
+    
+    num = length(ag_label)
+    print(num)
+    
+    if (num < 20) {
+      modifier = 3
+    } else {
+      modifier = 200
+    }
+    print(modifier)
+    return(650 + 5 * modifier)
+  }
+  
+  # visualization plot
+  output$distPlot <- renderPlot({
+    plot_Vis()
+  }, 
+  
+  width=850,height=myHeight)
+
+  
+  
+  # download plotVis
+  output$plotVis <- downloadHandler(
+    
+    filename <- "visualization.png",
+    content <- function(file){
+      dpi_val <- 85
+      h <- (myHeight()/dpi_val)
+      print(myHeight())
+      print(h)
+      ggsave(file, plot = plot_Vis(),device = "png",width=11, height=h,units="in",dpi=dpi_val)
+    }
+  )
+  
+ 
+  # download table
+  output$downloadTable <- downloadHandler(
+    filename = "table.csv",
+    content = function(file) {
+      write.csv(as.data.frame(data())[c(1:3,86,13,15,17,23,25,99,129)], file)
+    }
+  )
+  
+  # print total number of records selected (for reference)
+  output$n_records <- renderText({
+    nrow(data())
+  })
+
+  
+
+})
---
+++ ui.R
@@ -0,0 +1,83 @@
+# Data dashboard for Metro21 Fire Risk Analysis Project
+# Created for: Pittsburgh Bureau of Fire
+# Authors: Qianyi Hu, Michael Madaio
+# Latest update: January 25, 2018
+
+
+# This is the ui part of the dashboard
+
+#install.packages("shiny")
+#install.packages("markdown")
+library(shiny)
+library(markdown)
+
+shinyUI(fluidPage(
+  # sidebar
+  
+  sidebarLayout(
+    
+    
+    sidebarPanel(
+      # select x-axis in the visualization plot
+      
+      selectInput("yvar","Display",
+                  choices = c("Fire Risk Scores"),
+                  selected = "Fire Risk Scores",
+                  multiple = FALSE),
+      selectInput("xvar",label=NULL,
+                    choices = c("Property Classification","Property Usage Type","Neighborhood","Fire District"),
+                    selected = "Property Usage Type",
+                    multiple = FALSE),
+
+      
+      selectInput("property",
+                  label = "Filter by property type",
+                  choices = c("All Classification Types",as.vector(sort(unique(model$STATEDESC)))),
+                  selected = "All Classification Types",
+                  multiple=TRUE),
+      selectInput("use",
+                  label = NULL,
+                  choices = c("All Usage Types",as.vector(sort(unique(model$USEDESC)))),
+                  selected = "All Usage Types",
+                  multiple=TRUE),
+      selectInput("nbhood",
+                  label = "Filter by location",
+                  choices = c("All Neighborhoods", as.vector(sort(unique(model$geo_name_nhood)))),
+                  selected = "All Neighborhoods",
+                  multiple=TRUE),
+      selectInput("fire_dst",
+                  label = NULL,
+                  choices = c("All Fire Districts",as.vector(sort(unique(model$Pgh_FireDistrict)))),
+                  selected = "All Fire Districts",
+                  multiple=TRUE),
+      sliderInput("range",
+                  label="Risk range: (low to high)",
+                  min=1, max=10, value=c(2, 10)),
+    width=3),
+    
+    
+    
+    mainPanel(
+      # divided into 2 tab panels, visualization and table
+      tabsetPanel(
+        
+        # visualization tab includes the bar chart with selected options
+        tabPanel("Visualization",
+                 wellPanel(
+                   div(style="display: inline-block;vertical-align:top", p("Number of records selected:")),
+                   div(style="display: inline-block;vertical-align:top;text-align:center", textOutput("n_records")),
+                   div(style="display: inline-block;vertical-align:top;float:right", downloadButton("plotVis",label = "Download"))
+                 ),
+                 div(style="display: inline-block;vertical-align:top;float:right", plotOutput("distPlot"))
+                 ),
+        # the table tab includes the table with selected options (another format of the visualization with the same filtered data)
+        # it also has the download option to download the table as a csv file.
+        tabPanel("Table", 
+                 downloadButton('downloadTable', label='Download'),
+                 dataTableOutput("table")
+                 )
+        
+      )
+    )
+  ))
+)
